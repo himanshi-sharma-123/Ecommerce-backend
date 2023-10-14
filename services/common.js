47df4caf982c965e3886cb9b9c9ba8eab58a77e1
Himanshi Sharma <himanshish456@gmail.com>
2023-10-14 17:51:25 +0530

    tokens of user and admin updated

--- services/common.js
+++ services/common.js
@@ -19,11 +19,14 @@ exports.cookieExtractor = function (req) {
     token = req.cookies["jwt"];
   }
   // TODO: this is temporary token
+
+  //demo@gmail.com
   // token =
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MjI3YzNiMzBkMzQ0NzQ5YTg5M2M1NSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk2NzU4ODQzfQ.jNAlgqSQLurbsbbJO8fMdoQkENwYYrqFrMaXZnqSeAI";
-  // token =
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MjI2ZTgxN2Q5YzQyZTgyZjZmY2I3MCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk2NzgzMDMzfQ.vLXFMBhnK3S0pRp4I3fOMXpD9rvcWoIqGFF5oYYbobo";
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MjM5M2YzNjc4MmFiNTI4YjM5NTQxOSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk2ODMwNDU3fQ.F5t2OMNnrj1CYeFzasVOBi0smloz7GB4fB2saoBNAmU";
+
+  //admin@gmmail.com
   token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MjM5M2YzNjc4MmFiNTI4YjM5NTQxOSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk2ODMwNDU3fQ.F5t2OMNnrj1CYeFzasVOBi0smloz7GB4fB2saoBNAmU";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MmE4Njg0MzQ4Mjg1ZmE3OGM0Yzk1OSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk3Mjg1NzcwfQ.9podwB0bdSLJOrDfwyzpbmBxKpY8Ns2miuwHw82tA5c";
+
   return token;
 };
